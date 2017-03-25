01cddc94f5c66d1bf9cb721437c71b676b7ff397
Supriya Kunnoth -X (skunnoth - WIPRO LIMITED at Cisco) <skunnoth@partnet.cisco.com>
2017-03-25 18:54:45 +0530

    Removed table and Converted to JSON

--- angularS.js
+++ angularS.js
@@ -7,7 +7,7 @@ app.controller('myCtrl', function($scope,$http,$compile) {
 	var validationsArray = [];
 
 	function renderForm () {
-		var element = "<table class='table table-bordered'><form><tr><th>"+formData.data.form_id+"</th></tr>";
+		var element = "<form><label>"+formData.data.form_id+"</label>";
 		formData.data.form_fields.forEach(function (temp, index, arr) {
 			var tempId = "field"+index;
 			var tempElement = "";
@@ -15,11 +15,11 @@ app.controller('myCtrl', function($scope,$http,$compile) {
 			switch(temp.component){
 				case 'textinput':
 					tempElement = "";
-					element +=  "<tr><td><label>"+temp.label+"</label></td>";
+					element +=  "<fieldset><label>"+temp.label+"</label>";
 					if(temp.autofill){
-						tempElement = "<td><input type='text' id='"+tempId+"' name='"+tempId+"'  value='"+temp.autofill+"' readonly required><br></td></tr>";
+						tempElement = "<input type='text' id='"+tempId+"' name='"+temp.label+"'  value='"+temp.autofill+"' disabled required><br><fieldset>";
 					} else {
-						tempElement = "<td><input type='text' id='"+tempId+"'  name='"+tempId+"'><br></td></tr>";
+						tempElement = "<input type='text' id='"+tempId+"'  name='"+temp.label+"'><br><fieldset>";
 					}
 					element += tempElement;
 					if (temp.validation) {
@@ -31,11 +31,11 @@ app.controller('myCtrl', function($scope,$http,$compile) {
 					break;
 				case 'textarea':
 					tempElement = "";
-					element +=  "<tr><td><label>"+temp.label+"</label></td>";
+					element +=  "<fieldset><label>"+temp.label+"</label>";
 					if (temp.autofill != null) {
-						tempElement = "<td><textarea rows='4' cols='50' name='"+tempId+"' id='"+tempId+"'  readonly required>"+temp.autofill+"</textarea><br></td></tr>";
+						tempElement = "<textarea rows='4' cols='50' name='"+temp.label+"' id='"+tempId+"'  disabled required>"+temp.autofill+"</textarea><br><fieldset>";
 					} else {
-						tempElement = "<td><textarea rows='4' cols='50' name='"+tempId+"' id='"+tempId+"'></textarea><br></td></tr>";
+						tempElement = "<textarea rows='4' cols='50' name='"+temp.label+"' id='"+tempId+"'></textarea><br><fieldset>";
 					}
 					element += tempElement;
 					if (temp.validation) {
@@ -48,7 +48,7 @@ app.controller('myCtrl', function($scope,$http,$compile) {
 				case 'select':
 					var temArray =[];
 					temArray = temp.options.toString().split(',');
-					element +=  "<tr><td><label>"+temp.label+"</label></td><td><select name='"+tempId+"'>";
+					element +=  "<fieldset><label>"+temp.label+"</label><select name='"+temp.label+"'>";
 
 					if(temp.autoselect != null){
 						temArray.forEach(function(option){
@@ -65,34 +65,34 @@ app.controller('myCtrl', function($scope,$http,$compile) {
 							element += "<option value='"+option+"'>"+option+"</option>";
 						});
 					}
-					element += "</select></td></tr><br>";
+					element += "</select></fieldset>";
 					//console.log("select");
 					break;
 				case 'radio':
 					var temArray =[];
 					temArray = temp.options.toString().split(',');
-					element += 	"<tr><td><label>"+temp.label+"</label></td>";
+					element += 	"<fieldset><label>"+temp.label+"</label>";
 					if (temp.autoselect != null) {
 						temArray.forEach( function(option) {
 							if (option == temp.autoselect) {
-								element += "<td><input type='radio'  value='"+option+"' checked readonly>"+option+"<br></td>";
+								element += "<input type='radio' name='"+option+"' value='"+option+"' checked disabled>"+option+"<br>";
 							} else {
-								element += "<td><input type='radio'  value='"+option+"' disabled>"+option+"<br></td>";
+								element += "<input type='radio' name='"+option+"' value='"+option+"' disabled>"+option+"<br>";
 							}
 						});
 					} else {
 						temArray.forEach(function(option){
-							element += "<td><input type='radio'>"+option+"<br></td>";
+							element += "<input type='radio' name='"+option+"' value='"+option+"'>"+option+"<br>";
 						});
 					}
-					element += "<br></tr>"; 
+					element += "<br></fieldset>"; 
 					//console.log("radio");
 					break;
 				case 'checkbox': 
 					var auto=[];
 					var temArray =[];
 					temArray = temp.options.toString().split(',');
-					element += 	"<tr><td><label>"+temp.label+"</label></td>";
+					element += 	"<fieldset><label>"+temp.label+"</label>";
 					if(temp.autoselect != null){
 						var auto = temp.autoselect;
 						auto = auto.toString().split(',');
@@ -100,25 +100,25 @@ app.controller('myCtrl', function($scope,$http,$compile) {
 						console.log("temArray:"+temArray);
 						temArray.forEach(function(saveEach){
 							if(auto.indexOf(saveEach) != -1){
-								element += "<td><input type='checkbox' name='"+saveEach+"' disabled='disabled' checked='checked'>"+saveEach+"<br></td>";
+								element += "<input type='checkbox' name='"+saveEach+"' disabled='disabled' checked='checked'>"+saveEach+"<br>";
 							}
 							else{
-								element += "<td><input type='checkbox' name='"+saveEach+"' disabled='disabled'>"+saveEach+"<br></td>";
+								element += "<input type='checkbox' name='"+saveEach+"' disabled='disabled'>"+saveEach+"<br>";
 							}
 						});
 					}
 					else{
 						temArray.forEach(function(saveEach){
-							element += "<td><input type='checkbox'  name='"+saveEach+"'>"+saveEach+"<br></td>";
+							element += "<input type='checkbox'  name='"+saveEach+"'>"+saveEach+"<br>";
 						});
 					}
-					element+= "<br></tr>"; 
+					element+= "<br></fieldset>"; 
 					//console.log("checkbox");
 					break;
 			}
 		});
 
-		element += "<tr><td><input type='submit' value='submit' ng-click='submitFunc($event)'></td></tr></form></table>";
+		element += "<fieldset><input type='submit' value='submit' ng-click='submitFunc($event)'><fieldset></form>";
 
 		compiledElement = $compile(element)($scope);
 		$('#formDisplay').append(compiledElement);
@@ -146,26 +146,36 @@ app.controller('myCtrl', function($scope,$http,$compile) {
 		});
 	}
     $.fn.serializeObject = function() {
-	var o = {};
-	var a = this.serializeArray();
-	$.each(a, function() {
-		if (o[this.name]) {
-			if (!o[this.name].push) {
-				o[this.name] = [o[this.name]];
+		var o = {};
+		var a = this.serializeArray();
+		$.each(a, function() {
+			if (o[this.name]) {
+				if (!o[this.name].push) {
+					o[this.name] = [o[this.name]];
+				}
+				o[this.name].push(this.value || '');
+			} else {
+				o[this.name] = this.value || '';
 			}
-			o[this.name].push(this.value || '');
-		} else {
-			o[this.name] = this.value || '';
-		}
-	});
-	return o;
+		});
+		return o;
 	};
 	$scope.submitFunc = function($event){
 		var isValid = vaildateFields();
 
 		if (isValid) {
-			var data = JSON.stringify($('form').serializeObject());
-			//var data = ($('form').serializeJSON());
+
+			var form = $('form');
+
+			// Find disabled inputs, and remove the "disabled" attribute
+			var disabled = form.find(':disabled').removeAttr('disabled');
+
+			// serialize the form
+			var data = JSON.stringify(form.serializeObject());
+
+			// re-disabled the set of inputs that you previously enabled
+			disabled.attr('disabled','disabled');
+
 			console.log(data);
 			$('#formDisplay').empty();
 			var element = "<h1>Successfully Submitted</h1>";
